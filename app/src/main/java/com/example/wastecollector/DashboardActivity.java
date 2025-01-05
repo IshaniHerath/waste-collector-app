6eea1fb951f6ee5a82088dd8a96fb0781d1d420f
Ishani Herath <ishani.herath@softone.lk>
2025-01-05 10:13:48 +0100

    dashboard logic

--- app/src/main/java/com/example/wastecollector/DashboardActivity.java
+++ app/src/main/java/com/example/wastecollector/DashboardActivity.java
@@ -1,24 +1,65 @@
 package com.example.wastecollector;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class DashboardActivity extends AppCompatActivity {
+
+public class DashboardActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private Spinner locationDropDown;
+    private TextView txv_fullbins;
+    private TextView txv_readybins;
+    private TextView txv_notreadybins;
+    private TextView txv_unfilledbins;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+  EdgeToEdge.enable(this);
         setContentView(R.layout.dashboard_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.dashboard), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Initialize views
+        locationDropDown = findViewById(R.id.spinner);
+        txv_fullbins = (TextView) findViewById(R.id.tv_total_bins_value);
+        txv_readybins = (TextView) findViewById(R.id.tv_ready_collect_value);
+        txv_notreadybins = (TextView) findViewById(R.id.tv_ready_soon_value);
+        txv_unfilledbins = (TextView) findViewById(R.id.tv_unfilled_value);
+
+        // Set initial values to empty
+        txv_fullbins.setText("_");
+        txv_readybins.setText("_");
+        txv_notreadybins.setText("_");
+        txv_unfilledbins.setText("_");
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.locations,
+                android.R.layout.simple_spinner_item
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        locationDropDown.setAdapter(adapter);
+        locationDropDown.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+        String choice = adapterView.getItemAtPosition(position).toString();
+        Toast.makeText(getApplicationContext(), choice, Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
